727a385d24a49d65573a1decb38392e73d26468b
Patrick Rudolph <patrick.rudolph@9elements.com>
2023-07-12 09:26:31 +0200

    fix bios_update.sh
    merge with bios_update.
    
    Set flash security overwrite before turning on chassis.
    
    Change-Id: I209175b08eaadb97b7dd18c17f096f8eb6de8c51
    Signed-off-by: Patrick Rudolph <patrick.rudolph@9elements.com>

--- meta-ibm/meta-sbp1/recipes-phosphor/flash/phosphor-software-manager/bios-update.sh
+++ meta-ibm/meta-sbp1/recipes-phosphor/flash/phosphor-software-manager/bios-update.sh
@@ -19,52 +19,91 @@ ME_GET_DEVICE_ID="1 6 0 0x1 0"
 
 echo "Bios upgrade started at $(date)"
 
-#Power off host server.
-echo "Power off host server"
-busctl set-property xyz.openbmc_project.State.Host0 /xyz/openbmc_project/state/host0 xyz.openbmc_project.State.Host RequestedHostTransition s "xyz.openbmc_project.State.Host.Transition.Off"
-echo "Host server powered off"
+power_status() {
+    st=$(busctl get-property xyz.openbmc_project.State.Chassis /xyz/openbmc_project/state/chassis0 xyz.openbmc_project.State.Chassis CurrentPowerState | cut -d"." -f6)
+    if [ "$st" == "On\"" ]; then
+        echo "on"
+    else
+        echo "off"
+    fi
+}
 
-sleep 1
+power_off() {
+    echo "Shutting down Server"
+    busctl set-property xyz.openbmc_project.State.Chassis /xyz/openbmc_project/state/chassis0 xyz.openbmc_project.State.Chassis RequestedPowerTransition s xyz.openbmc_project.State.Chassis.Transition.Off
+    for i in {1..10}; do
+      if [ "$(power_status)" == "off" ]; then
+        break
+      else
+        sleep 1
+      fi
+    done
 
-echo "Power on PCH"
-busctl set-property xyz.openbmc_project.State.Chassis /xyz/openbmc_project/state/chassis0 xyz.openbmc_project.State.Chassis RequestedPowerTransition s "xyz.openbmc_project.State.Chassis.Transition.On"
-echo "PCH is powered on"
+    if [ "$(power_status)" != "off" ]; then
+      echo "Failed to power off Server"
+      exit 1
+    fi
+}
 
-for i in {1..10}; do
-  st=$(busctl get-property xyz.openbmc_project.State.Chassis /xyz/openbmc_project/state/chassis0 xyz.openbmc_project.State.Chassis CurrentPowerState | cut -d"." -f6)
-  if [ "$st" == "On\"" ]; then
-    break
-  else
-    sleep 1
-  fi
-done
+power_on() {
+    echo "Powering on Server"
+    busctl set-property xyz.openbmc_project.State.Chassis /xyz/openbmc_project/state/chassis0 xyz.openbmc_project.State.Chassis RequestedPowerTransition s xyz.openbmc_project.State.Chassis.Transition.On
+    for i in {1..10}; do
+      if [ "$(power_status)" == "on" ]; then
+        break
+      else
+        sleep 1
+      fi
+    done
 
-if [ "$st" != "On\"" ]; then
-  echo "Failed to power on PCH"
-  exit 1
-fi
+    if [ "$(power_status)" != "on" ]; then
+      echo "Failed to power on Server"
+      exit 1
+    fi
+}
 
-echo "Wait for ME firmware to start"
-for i in {1..10}; do
-  if ! busctl call --timeout=1 "$IPMB_OBJ" "$IPMB_PATH" "$IPMB_INTF" $IPMB_CALL $ME_GET_DEVICE_ID 2>/dev/null; then
-    sleep 1
-  else
-    break
-  fi
-done
+me_wait_poweron() {
+    echo "Wait for ME firmware to start"
+    for i in {1..10}; do
+      if ! busctl call --timeout=1 "$IPMB_OBJ" "$IPMB_PATH" "$IPMB_INTF" $IPMB_CALL $ME_GET_DEVICE_ID 2>/dev/null; then
+        sleep 1
+      else
+        break
+      fi
+    done
 
-if ! busctl call --timeout=1 "$IPMB_OBJ" "$IPMB_PATH" "$IPMB_INTF" $IPMB_CALL $ME_GET_DEVICE_ID 2>/dev/null; then
-  echo "Failed to communicate with SPS firmware"
-  exit 1
-fi
+    if ! busctl call --timeout=1 "$IPMB_OBJ" "$IPMB_PATH" "$IPMB_INTF" $IPMB_CALL $ME_GET_DEVICE_ID 2>/dev/null; then
+      echo "Failed to communicate with SPS firmware"
+      exit 1
+    fi
+}
 
-#Set ME to recovery mode
-echo "Set ME to recovery mode"
-# shellcheck disable=SC2086
-busctl call "$IPMB_OBJ" "$IPMB_PATH" "$IPMB_INTF" $IPMB_CALL $ME_CMD_RECOVER
+me_force_recovery_mode() {
+    # Set ME to recovery mode
+    echo "Set ME to recovery mode"
+    # shellcheck disable=SC2086
+    busctl call "$IPMB_OBJ" "$IPMB_PATH" "$IPMB_INTF" $IPMB_CALL $ME_CMD_RECOVER
+}
+
+me_reset() {
+    #Reset ME to boot from new bios
+    echo "Reset ME to boot from new firmware"
+    # shellcheck disable=SC2086
+    busctl call "$IPMB_OBJ" "$IPMB_PATH" "$IPMB_INTF" $IPMB_CALL $ME_CMD_RESET
+}
+
+# Power off PCH
+power_off
 
 # Enable FM_FLASH_SEC_OVRD
 gpioset $(gpiofind FM_FLASH_SEC_OVRD)=1
+
+# Power on PCH
+power_on
+
+me_wait_poweron
+
+me_force_recovery_mode
 
 #Flashcp image to device.
 if [ -e "$IMAGE_FILE" ];
@@ -75,16 +114,16 @@ else
     echo "Bios image $IMAGE_FILE doesn't exist"
 fi
 
-# Disable FM_FLASH_SEC_OVRD
-gpioset $(gpiofind FM_FLASH_SEC_OVRD)=0
+# Reset ME
+me_reset
 
-#Reset ME to boot from new bios
-echo "Reset ME to boot from new bios"
-# shellcheck disable=SC2086
-busctl call "$IPMB_OBJ" "$IPMB_PATH" "$IPMB_INTF" $IPMB_CALL $ME_CMD_RESET
 sleep 5
 
-busctl set-property xyz.openbmc_project.State.Chassis /xyz/openbmc_project/state/chassis0 xyz.openbmc_project.State.Chassis RequestedPowerTransition s xyz.openbmc_project.State.Chassis.Transition.Off
+# Power off PCH
+power_off
+
+# Disable FM_FLASH_SEC_OVRD
+gpioset $(gpiofind FM_FLASH_SEC_OVRD)=0
 
 # Delete cached bios version file if it exist.
 rm /var/cache/bios_version || true
