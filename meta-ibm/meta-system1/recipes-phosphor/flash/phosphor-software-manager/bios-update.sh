fa4e54fdd5094d08b2bea291e55702fb23647c19
NINAD PALSULE <ninadpalsule@us.ibm.com>
2024-10-25 15:36:28 -0500

    flashrom: Correct the gpio line name (#5421)
    The gpio line flash-write-override is used to flash override.
    
    Change-Id: I8612b5a43213fd060a272cec47193865409e8757
    
    Signed-off-by: Ninad Palsule <ninad@linux.ibm.com>

--- meta-ibm/meta-system1/recipes-phosphor/flash/phosphor-software-manager/bios-update.sh
+++ meta-ibm/meta-system1/recipes-phosphor/flash/phosphor-software-manager/bios-update.sh
@@ -68,57 +68,10 @@ me_reset() {
     busctl call "$IPMB_OBJ" "$IPMB_PATH" "$IPMB_INTF" sendRequest yyyyay "${ME_CMD_RESET[@]}"
 }
 
-configure_flash_env() {
-    # 1. Assert PCH RESET
-    # 2. Enable/Disable FM_FLASH_SEC_OVRD
-    # 3. De-assert PCH RESET
-    local action="$1"
-    if [ "$action" == "enable" ]; then
-        echo "Asserting PCH RESET and enabling flash write override"
-        gpioset "${PCH_RESET_GPIO_CHIP}" "${PCH_RESET_GPIO_LINE}=0"
-        gpioset "${FLASH_OVERRIDE_GPIO_CHIP}" "${FLASH_OVERRIDE_GPIO_LINE}=1"
-        gpioset "${PCH_RESET_GPIO_CHIP}" "${PCH_RESET_GPIO_LINE}=1"
-        sleep 2
-    elif [ "$action" == "disable" ]; then
-        echo "Disabling flash write override and resetting PCH RESET"
-        gpioset "${PCH_RESET_GPIO_CHIP}" "${PCH_RESET_GPIO_LINE}=0"
-        gpioset "${FLASH_OVERRIDE_GPIO_CHIP}" "${FLASH_OVERRIDE_GPIO_LINE}=0"
-        gpioset "${PCH_RESET_GPIO_CHIP}" "${PCH_RESET_GPIO_LINE}=1"
-    else
-        echo "Invalid action specified for configure_flash_env. Use 'enable' or 'disable'."
-        exit 1
-    fi
-}
-
-cleanup_env() {
-    # Disable flash protection and clean up
-    configure_flash_env "disable"
-}
-
-reset_and_cleanup_env() {
-    echo "Reset ME and disable flash-write-override GPIO"
-    me_reset
-    sleep 5
-    cleanup_env
-}
-
-
-PCH_RESET_GPIO_PIN=$(gpiofind "pch-reset")
-if [ -z "${PCH_RESET_GPIO_PIN}" ]; then
-    echo "gpio 'pch-ready' not found in device tree. Exiting."
-    exit 0
-fi
-read -r PCH_RESET_GPIO_CHIP PCH_RESET_GPIO_LINE <<< "$PCH_RESET_GPIO_PIN"
-
-FLASH_OVERRIDE_GPIO_PIN="$(gpiofind flash-write-override)"
-if [ -z "${FLASH_OVERRIDE_GPIO_PIN}" ]; then
-    echo "gpio 'flash-write-override' not found in device tree. Exiting."
-    exit 0
-fi
-read -r FLASH_OVERRIDE_GPIO_CHIP FLASH_OVERRIDE_GPIO_LINE <<< "$FLASH_OVERRIDE_GPIO_PIN"
-
-configure_flash_env "enable"
-sleep 2
+# Enable FM_FLASH_SEC_OVRD
+gpio_info="$(gpiofind flash-write-override)"
+read -r gpiochip gpio_line <<< "$gpio_info"
+gpioset "${gpiochip}" "${gpio_line}=1"
 
 me_wait_poweron
 
